6bca11f2688334b3daf00f61d6d0ba1fc0522038
shreya1704 <33744933+shreya1704@users.noreply.github.com>
2024-04-24 15:40:05 -0400

    update

--- R/sc_bispecific_expression.R
+++ R/sc_bispecific_expression.R
@@ -1,12 +1,157 @@
-source("scbsabs_coexpress.R")
-source("scbsabs_plot.R")
+#source("scbsabs_coexpress.R")
+#source("scbsabs_plot.R")
 
 library(ggplot2)
 library(dplyr)
 library(Seurat)
+library(rlang)
+
+plot_seurat_data <- function(so, ident, geneA, geneB,geneA_present,geneB_present) {
+  # Subset Seurat object to remove cells with NA in the identity column
+  if (ident %in% colnames(so@meta.data)) {
+    so[[ident]][is.na(so[[ident]])] <- "Unknown"
+    Idents(so) <- so@meta.data[,ident]  # Set the identity column
+  } else {
+    stop("Identity column '", ident, "' not found in metadata.")
+  }
+  # Step 3: Plotting
+  #file=rlang::expr_text(substitute(so))
+  #pdf_file <- paste0("Coexpress_",file,"_",geneA, "_", geneB, ".pdf")
+  pdf_file <- paste0("Coexpress_",geneA, "_", geneB, ".pdf")
+  #wd= getwd()
+  pdf(pdf_file, width = 6, height = 6)
+  P0 <- DimPlot(so, label = TRUE)
+  print(P0)
+  if (geneA_present) {
+    P1 <- FeaturePlot(so, features = geneA)
+    print(P1)
+    D1 <- DotPlot(so, features = geneA) + RotatedAxis() + coord_flip() +
+      theme(axis.text.x = element_text(size = 5, angle = 45),
+            axis.text.y = element_text(size = 5),
+            legend.title = element_text(size = 5),
+            legend.text = element_text(size = 5))
+    print(D1)
+    #return(c(D1, P1))
+  } else {
+    P1 <- NULL  # Set to NULL if gene A is not present
+    D1 <- NULL
+  }
+
+  if (geneB_present) {
+    P2 <- FeaturePlot(so, features = geneB)
+    print(P2)
+    D2 <- DotPlot(so, features = geneB) + RotatedAxis() + coord_flip() +
+      theme(axis.text.x = element_text(size = 5, angle = 45),
+            axis.text.y = element_text(size = 5),
+            legend.title = element_text(size = 5),
+            legend.text = element_text(size = 5))
+    print(D2)
+  } else {
+    P2 <- NULL  # Set to NULL if gene B is not present
+    D2 <- NULL
+  }
+
+  if (geneA_present & geneB_present) {
+    D3 <- DotPlot(so, features = c(geneA, geneB)) + RotatedAxis() + coord_flip() +
+      theme(axis.text.x = element_text(size = 5, angle = 45),
+            axis.text.y = element_text(size = 5),
+            legend.title = element_text(size = 5),
+            legend.text = element_text(size = 5))
+    print(D3)
+  } else {
+    D3 <- NULL
+  }
+
+  dev.off()
+  message("Plots saved to ", pdf_file, ".")
+}
+
+make_boolean_matrix <- function(so) {
+  counts=so@assays$RNA$counts
+  rownames(counts)=rownames(so@assays$RNA)
+  colnames(counts)=colnames(so@assays$RNA)
+  subset_counts <- counts[1:nrow(counts), 1:ncol(counts)]
+  binary_df <- as.data.frame(subset_counts > 0)
+  binary_df <- as.data.frame(t(binary_df))
+  binary_df <- binary_df[order(rownames(binary_df)), ]
+  return(binary_df)
+}
+
+
+
+calculate_percentages2 <- function(df, geneA, geneB)
+{
+  if (geneA %in% colnames(df)) {
+    num_A <- sum(df[[geneA]] == TRUE)
+    A_names <- if (num_A > 0) {
+      rownames(df[df[[geneA]], , drop = FALSE])
+    } else {
+      NULL
+    }
+
+  }else
+  {
+    num_A=0
+    num_AB=0
+  }
+
+  if (geneB %in% colnames(df)) {
+    num_B <- sum(df[[geneB]] == TRUE)
+    B_names <- if (num_B > 0) {
+      rownames(df[df[[geneB]], , drop = FALSE])
+    } else {
+      NULL
+    }
+
+  }else
+  {
+    num_B=0
+    num_AB=0
+  }
+
+  num_AB <- ifelse(num_A > 0 && num_B > 0, length(intersect(A_names, B_names)), 0)
+
+  total <- nrow(df)
+
+  percent_A <- round((num_A / total) * 100, 2)
+  percent_B <- round((num_B / total) * 100, 2)
+  percent_AB <- round((num_AB / total) * 100, 2)
+
+  return(c(percent_A = percent_A, percent_B = percent_B, percent_AB = percent_AB))
+}
+
+coexpression_calculation<- function(binary_df, geneA, geneB)
+{
+  result_df <- data.frame(
+    Cell_Type = character(),
+    Percent_Expressing_A = numeric(),
+    Percent_Expressing_B = numeric(),
+    Percent_Coexpressing_A_B = numeric(),
+    stringsAsFactors = FALSE
+  )
+
+  for (ctype in unique(binary_df$Ident)) {
+
+    subset=binary_df[binary_df$Ident==ctype,]
+    res=calculate_percentages2(subset, geneA, geneB)
+    new_row <- data.frame(
+      Cell_Type = ctype,
+      Percent_Expressing_A = res[["percent_A"]],
+      Percent_Expressing_B = res[["percent_B"]],
+      Percent_Coexpressing_A_B = res[["percent_AB"]],
+      stringsAsFactors = FALSE
+    )
+
+    # Bind the new row to result_df
+    result_df <- rbind(result_df, new_row)
+
+  }
+
+  return(result_df)
+}
 
 sc_bispecific_expression <- function(so, ident, geneA, geneB) {
-  
+
 # Step 1: Check gene presence
 geneA_present <- geneA %in% rownames(so@assays$RNA$counts)
 geneB_present <- geneB %in% rownames(so@assays$RNA$counts)
@@ -25,7 +170,7 @@ if (!geneA_present || !geneB_present) {
   missing_genes <- c()
   if (!geneA_present) missing_genes <- c(missing_genes, geneA)
   if (!geneB_present) missing_genes <- c(missing_genes, geneB)
-  
+
   warning("The given gene not found in the seurat dataset: ", paste(missing_genes, collapse = ", "))
 }
 
@@ -33,22 +178,24 @@ if (!geneA_present || !geneB_present) {
 so_name=rlang::expr_text(substitute(so))
 variable_name <- paste0(so_name, "_boolean")
 
-#assign(variable_name, make_boolean_matrix(so, ident), envir = .GlobalEnv)
-
 if (!exists(variable_name, envir = .GlobalEnv)){
   print("generating boolean matrix")
-  boolean_matrix <- make_boolean_matrix(so, ident)
+  boolean_matrix <- make_boolean_matrix(so)
   assign(variable_name, boolean_matrix, envir = .GlobalEnv)
-  #assign(variable_name, make_boolean_matrix(so, ident), envir = .GlobalEnv)
   print("Boolean matrix generated.....")
 }
 
+bool_df=get(variable_name)
+Ident_col <- as.data.frame(so@meta.data[, ident, drop = FALSE])
+Ident_col <- Ident_col[order(rownames(Ident_col)), , drop = FALSE]
+bool_df <- cbind(bool_df, Ident_col)
+colnames(bool_df)[ncol(bool_df)] <- "Ident"
+bool_df <- bool_df[!is.na(bool_df$Ident), ]
 
 message("Calculating percentage of cells expressing given gene(s).....")
 
-result_df <- coexpression_calculation(get(variable_name), geneA, geneB)
+result_df <- coexpression_calculation(bool_df, geneA, geneB)
 output_name <- paste0("Coexpress_", so_name, "_", geneA, "_", geneB)
-#assign(output_name, result_df, envir = .GlobalEnv)
 write.csv(result_df,paste0(output_name,".csv"),row.names = F,quote=F)
 
 message("Coexpression matrix saved to ", output_name, ".csv.")
